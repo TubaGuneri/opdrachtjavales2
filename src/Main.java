7155091e4256ca2cd563d7caa60a67f9d996f5e3
TubaGuneri <t_guneri@hotmail.com>
2024-03-19 22:50:19 +0100

    klaar voor commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
-        applePieRecipe.stappenPlan();
-        applePieRecipe.ingredientenLijst();
+//        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+//        applePieRecipe.stappenPlan();
+//        applePieRecipe.ingredientenLijst();
 
         extraOpdracht2 extraOpdracht2 = new extraOpdracht2();
         extraOpdracht2.tunaSalad();
